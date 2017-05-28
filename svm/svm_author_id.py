0177fc10cbb82ccd2df773c436a31f6d4444b657
Sylwia Üçüncü <sylwia_uc@yahoo.com>
2017-05-28 06:57:13 +0200

    Naive Bayes

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,6 +19,25 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+#
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+clf = svm.SVC(kernel='rbf', C=10000.0)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+print "accuracy:", accuracy_score(pred, labels_test)
+
+
+
 
 
 
